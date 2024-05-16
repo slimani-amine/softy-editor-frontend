b86bd86dfb7aff088909e3c2cca3f80aaebcd138
slimani-amine <slimanimedamine24@gmail.com>
2024-05-16 09:30:08 +0100

    change the logo dark mode

--- src/shared/components/Shared/Icons/Logo.tsx
+++ src/shared/components/Shared/Icons/Logo.tsx
@@ -1,8 +1,8 @@
 const Logo = () => {
   return (
     <>
-      <img src="/Logo.png" alt="logo" className=" w-36 dark:hidden " />
-      <img src="LogoDark.png" alt="logo" className=" w-36 hidden dark:block" />
+      <img src="/Logo.png" alt="logo" className=" w-36  " />
+      {/* <img src="LogoDark.png" alt="logo" className=" w-36 hidden dark:block" /> */}
     </>
   );
 };
